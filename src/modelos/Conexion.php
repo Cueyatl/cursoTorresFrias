bf4ce0f04d900ae19aec9d60cf337ef8fb66a987
Tlayolli <demiliano.aguilar@gmail.com>
2024-03-12 14:36:17 -0600

    Deleted cards, advance in store and modication of navbar

--- src/modelos/Conexion.php
+++ src/modelos/Conexion.php
@@ -1,20 +1,18 @@
 <?php
-// al chile no se que hace esto.
-  class Conexion{
+    class Conexion{
+        private $HOST = "127.0.0.1";
+        private $DB   = "cursocuc";
+        private $USER = "unicuc";
+        private $PWD  = "1234";
 
-    $HOST = "192.168.100.232";
-    $DB="CURSOCUC";
-    $USER="unicuc";
-    $PWD="1234";
+        function __construct(){}
+        function crearConexion(){
+            $mysqli = new mysqli($this->HOST,$this->USER,$this->PWD,$this->DB);
+            if($mysqli->connect_errno){
+                return null;
+            }
+            return $mysqli;
+        }
 
-    function __construct(){}
-    function crearConexion(){
-      $mysqli = new mysqli($this->HOST,$this->USER,$this->PWD,$this->DB);
-      if($mysqli->connect_errno){
-          return null;
-      }
-      return $mysqli;
     }
-  }
-
-  
+?>
